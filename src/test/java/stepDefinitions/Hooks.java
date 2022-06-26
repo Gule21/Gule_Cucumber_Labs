aafc43c0056382a1f84ad8c2168b205381d60e79
Gule21 <gulebatgi@hotmail.com>
2022-06-25 23:28:47 -0400

    Cucumber labs

---
+++ src/test/java/stepDefinitions/Hooks.java
@@ -0,0 +1,28 @@
+package stepDefinitions;
+
+import io.cucumber.java.Scenario;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import utils.BrowserUtils;
+import utils.Driver;
+
+public class Hooks {
+    @Before
+    public void setup(){
+        Driver.getDriver();
+        BrowserUtils.setWaitTime();
+        Driver.getDriver().manage().window().maximize();
+    }
+    @After
+    public void tearDown(Scenario scenario){
+        if(scenario.isFailed()){
+            byte[] data=((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(data, "image/png", scenario.getName());
+        }
+        Driver.closeDriver();
+
+    }
+
+}
---
+++ src/test/java/stepDefinitions/Steps.java
@@ -0,0 +1,53 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.support.PageFactory;
+import pages.Home;
+import utils.BrowserUtils;
+import utils.ConfigurationsReader;
+import utils.Driver;
+
+public class Steps extends BrowserUtils{
+    public Steps() {
+        PageFactory.initElements(Driver.getDriver(), this);}
+    Home home=new Home();
+    @Given("The user wants to see Guru demo site")
+    public void the_user_wants_to_see_guru_demo_site() {
+        Driver.getDriver().get(ConfigurationsReader.getProperties("demoURL"));
+        BrowserUtils.waitForPageToLoad(4);
+    }
+
+    @When("The user wants to Add Tariff Plan")
+    public void the_user_wants_to_add_tariff_plan() {
+        home.setAddTariff();
+
+    }
+
+    @Then("The user wants to verify {string} headerr")
+    public void the_user_wants_to_verify_headerr(String string) {
+        home.setVerifyheader("Add Tariff Plans");
+
+    }
+
+    @Then("The user wants to Add Tariff Plan details as")
+    public void the_user_wants_to_add_tariff_plan_details_as(io.cucumber.datatable.DataTable dataTable) {
+        home.setAddTariff();
+
+    }
+
+    @Then("The user wants to submit")
+    public void the_user_wants_to_submit() {
+        home.setSubmit();
+
+    }
+
+    @Then("The user wants to verify message as {string}")
+    public void the_user_wants_to_verify_message_as(String string) {
+        home.setVerifymsg("Congratulation you add Tariff Plan");
+
+    }
+
+}
+
